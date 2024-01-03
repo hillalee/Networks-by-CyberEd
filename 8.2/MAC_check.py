85189c50204602307a5978fc056ca290cc2e6d44
hilalee <146629145+hillalee@users.noreply.github.com>
2024-01-03 11:11:35 +0200

    Create MAC_check.py

---
+++ 8.2/MAC_check.py
@@ -0,0 +1,27 @@
+from scapy.all import *
+VALID_LEN = 17
+
+
+def valid_MAC(address):
+	rc = True
+
+	add_split = address.split(":")
+	valid_split = all(len(byte) == 2 for byte in add_split)
+
+	if len(address) != VALID_LEN or not valid_split:
+		rc = False
+
+	return rc
+
+while True:
+	user_MAC = input("Enter MAC address: \n")
+
+	# loop
+	if user_MAC == "exit":
+		break
+	if valid_MAC(user_MAC):
+		print(f"""MAC Address: '{user_MAC}' is valid \n""")
+	else:
+		print(f"""MAC Address: '{user_MAC}' is invalid \n""")
+
+
